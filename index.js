5ae687911f432053071d06988155815d28bafe31
cotto89 <cottto89@gmail.com>
2016-08-21 21:28:34 +0900

    add:  module to index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Composer = require('./dist/composer').default;
+const Store = require('./dist/store').default;
 const dispatcher = require('./dist/dispatcher').default;
 
 module.exports = {
   Composer,
+  Store,
   dispatcher,
 };
